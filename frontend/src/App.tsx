83e4f49a48a4e6973b63725d5cdf1a450f5d0496
shifaltz <pedroalmeidasantos.00@gmail.com>
2023-09-23 17:58:53 -0300

    feat: Create screen of Register Event

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RegisterEvents from './pages/RegisterEvents';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={ <Home /> }/>
       <Route path="/login" element={ <Login /> }/>
       <Route path="/register" element={ <Register /> }/>
+      <Route path="/registerEvents" element={ <RegisterEvents /> }/>
     </Routes>
     </>
   )
